e7ffda430883990d42c80ed29353798deaacf7fa
cuijianlei <thundersmile@126.com>
2019-10-18 14:03:24 +0800

    over

--- src/components/CountTo/index.tsx
+++ src/components/CountTo/index.tsx
@@ -4,4 +4,33 @@ import CountUp from 'countup';
 @Component({
   name: 'CountTo',
 })
-export default class CountTo extends Vue {}
+export default class CountTo extends Vue {
+  public counter!: CountUp;
+  @Prop({type: Number, default: 0}) public readonly start!: number;
+  @Prop(Number) public readonly end!: number;
+  @Emit('on-click')
+  public click(event) {
+    return event;
+  }
+
+  public get eleId() {
+    return `count_to_${(this as any)._uid}`;
+  }
+
+  public update(endVal: number): void {
+    this.counter.update(endVal);
+  }
+
+  protected render() {
+    return (
+      <div class='count-up-wrap' on-click={this.click}>
+        <span id={this.eleId}></span>
+      </div>
+    );
+  }
+
+  protected mounted() {
+    this.counter = new CountUp(this.eleId, this.start, this.end, 0, 1, {});
+    this.counter.start();
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="home">
+    <count-to @on-click="handleClick" ref="counter" :end="10000"></count-to>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import CountTo from '@/components/CountTo';
 
 @Component({
   name: 'HomePage',
+  components: {
+    CountTo,
+  },
 })
 export default class Home extends Vue {
+  protected mounted() {
+    setInterval(() => {
+      (this.$refs.counter as CountTo).update(Math.random() * 10000); // 这样每2秒钟这里会更新一次，更新的值是随机的
+    }, 2000);
+  }
+
+  public handleClick(event) {
+    console.log(event);
+  }
 }
 </script>
